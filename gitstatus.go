ebba9f8a2ef8c494fbfe6a2b16c72391dae0a8da
yukimemi <yukimemi@gmail.com>
2014-03-22 14:20:04 +0900

    search name changes to argument

--- gitstatus.go
+++ gitstatus.go
@@ -1,7 +1,6 @@
 package main
 
 import ( // {{{
-	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -41,31 +40,49 @@ func main() { // {{{
 	root, e := os.Getwd()
 	failOnError(e)
 
+	var searchName string
+	if len(os.Args) > 1 {
+		searchName = os.Args[1]
+	} else {
+		searchName = "."
+	}
+
 	fmt.Println("root", root)
 
-	e = filepath.Walk(root, walkFn)
+	e = printGitStatus(root, searchName)
 	failOnError(e)
 
 } // }}}
 
-func walkFn(path string, info os.FileInfo, err error) error {
+func printGitStatus(root string, searchName string) error { // {{{
 
-	m, e := regexp.MatchString(`\.git$`, info.Name())
-	if m && info.IsDir() {
-		os.Chdir(path)
-		os.Chdir("..")
-		cmd := exec.Command("git", "remote", "-v")
-		out, e := cmd.Output()
-		failOnError(e)
-		buf := bytes.NewBuffer(out)
-		stdout := buf.String()
-		if m, _ := regexp.MatchString("yukimemi", stdout); m {
-			cmd := exec.Command("git", "status")
+	walkFn := func(path string, info os.FileInfo, err error) error {
+
+		m, e := regexp.MatchString(`\.git$`, info.Name())
+		if m && info.IsDir() {
+			os.Chdir(path)
+			os.Chdir("..")
+			cmd := exec.Command("git", "remote", "-v")
 			out, e := cmd.Output()
 			failOnError(e)
-			fmt.Println(path)
-			fmt.Printf("%s\n", out)
+
+			if m, _ := regexp.MatchString(searchName, string(out)); m {
+				cmd := exec.Command("git", "status")
+				out, e := cmd.Output()
+				failOnError(e)
+				if n, _ := regexp.MatchString("nothing to commit, working directory clean", string(out)); !n {
+					fmt.Println("--------------------------------------------------------------------------------")
+					path, _ := os.Getwd()
+					fmt.Println("★ ", path)
+					fmt.Println(string(out))
+					fmt.Println("--------------------------------------------------------------------------------\n")
+				}
+			}
 		}
+		return e
 	}
+	e := filepath.Walk(root, walkFn)
+
 	return e
-}
+
+} // }}}
